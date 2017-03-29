62f53f118ceeb64c134f6b4c1a0700eeb882ff49
Jonathan Samudio <jonathan@prolificinteractive.com>
2017-03-29 16:26:32 -0400

    Added tests for the tilde tag.

--- Example/Tests/ParserTests.swift
+++ Example/Tests/ParserTests.swift
@@ -44,25 +44,40 @@ class ParserTests: XCTestCase {
         }
     }
     
-    // tilde test
+    func testParseStrikeThroughTags() {
+        do {
+            let (parsedString, parsedTags) = try MarkdownParser.parse("~~abc~~ def ~~ghi~~")
+            
+            XCTAssert(parsedString == "abc def ghi")
+            XCTAssert(parsedTags.count == 2)
+            
+            parsedTags.forEach { XCTAssert($0.isStrikeThoughTag()) }
+            
+            XCTAssert(parsedTags[0].range() == parsedString.range(of: "abc"))
+            XCTAssert(parsedTags[1].range() == parsedString.range(of: "ghi"))
+        } catch {
+            XCTFail("Parsing failed.")
+        }
+    }
     
-    // Update
     func testParseMixedTags() {
         do {
-            let (parsedString, parsedTags) = try MarkdownParser.parse("*abc* __def__ _ghi_ **jkl**")
+            let (parsedString, parsedTags) = try MarkdownParser.parse("*abc* __def__ _ghi_ **jkl** ~~mno~~")
             
-            XCTAssert(parsedString == "abc def ghi jkl")
-            XCTAssert(parsedTags.count == 4)
+            XCTAssert(parsedString == "abc def ghi jkl mno")
+            XCTAssert(parsedTags.count == 5)
             
             XCTAssert(parsedTags[0].isEmTag())
             XCTAssert(parsedTags[1].isStrongTag())
             XCTAssert(parsedTags[2].isEmTag())
             XCTAssert(parsedTags[3].isStrongTag())
+            XCTAssert(parsedTags[4].isStrikeThoughTag())
             
             XCTAssert(parsedTags[0].range() == parsedString.range(of: "abc"))
             XCTAssert(parsedTags[1].range() == parsedString.range(of: "def"))
             XCTAssert(parsedTags[2].range() == parsedString.range(of: "ghi"))
             XCTAssert(parsedTags[3].range() == parsedString.range(of: "jkl"))
+            XCTAssert(parsedTags[4].range() == parsedString.range(of: "mno"))
         } catch {
             XCTFail("Parsing failed.")
         }
@@ -84,6 +99,17 @@ class ParserTests: XCTestCase {
             let (parsedString, parsedTags) = try MarkdownParser.parse("< ( ^ _ ^ < )")
             
             XCTAssert(parsedString == "< ( ^ _ ^ < )")
+            XCTAssert(parsedTags.count == 0)
+        } catch {
+            XCTFail("Parsing failed.")
+        }
+    }
+    
+    func testLiteralTilde() {
+        do {
+            let (parsedString, parsedTags) = try MarkdownParser.parse("appox. ~9000")
+            
+            XCTAssert(parsedString == "appox. ~9000")
             XCTAssert(parsedTags.count == 0)
         } catch {
             XCTFail("Parsing failed.")
@@ -155,6 +181,15 @@ private extension MarkdownTag {
     func isStrongTag() -> Bool {
         switch self {
         case .strong(_):
+            return true
+        default:
+            return false
+        }
+    }
+    
+    func isStrikeThoughTag() -> Bool {
+        switch self {
+        case .strikeThough(_):
             return true
         default:
             return false
