b285c752691e978959ac847a1f9073b1db680a7e
Harlan Kellaway <harlan@prolificinteractive.com>
2017-07-07 17:39:34 -0400

    Added parser tests

--- Example/Tests/ParserTests.swift
+++ Example/Tests/ParserTests.swift
@@ -60,24 +60,42 @@ class ParserTests: XCTestCase {
         }
     }
     
+    func testParseUnderlinedElements() {
+        do {
+            let (parsedString, parsedElements) = try MarkdownParser.parse("==abc== def ==ghi==")
+            
+            XCTAssert(parsedString == "abc def ghi")
+            XCTAssert(parsedElements.count == 2)
+            
+            parsedElements.forEach { XCTAssert($0.isUnderlinedElement()) }
+            
+            XCTAssert(parsedElements[0].range == parsedString.range(of: "abc"))
+            XCTAssert(parsedElements[1].range == parsedString.range(of: "ghi"))
+        } catch {
+            XCTFail("Parsing failed.")
+        }
+    }
+    
     func testParseMixedElements() {
         do {
-            let (parsedString, parsedElements) = try MarkdownParser.parse("*abc* __def__ _ghi_ **jkl** ~~mno~~")
+            let (parsedString, parsedElements) = try MarkdownParser.parse("*abc* __def__ _ghi_ **jkl** ~~mno~~ ==pqr==")
             
-            XCTAssert(parsedString == "abc def ghi jkl mno")
-            XCTAssert(parsedElements.count == 5)
+            XCTAssert(parsedString == "abc def ghi jkl mno pqr")
+            XCTAssert(parsedElements.count == 6)
             
             XCTAssert(parsedElements[0].isEmElement())
             XCTAssert(parsedElements[1].isStrongElement())
             XCTAssert(parsedElements[2].isEmElement())
             XCTAssert(parsedElements[3].isStrongElement())
             XCTAssert(parsedElements[4].isStrikethroughElement())
+            XCTAssert(parsedElements[5].isUnderlinedElement())
             
             XCTAssert(parsedElements[0].range == parsedString.range(of: "abc"))
             XCTAssert(parsedElements[1].range == parsedString.range(of: "def"))
             XCTAssert(parsedElements[2].range == parsedString.range(of: "ghi"))
             XCTAssert(parsedElements[3].range == parsedString.range(of: "jkl"))
             XCTAssert(parsedElements[4].range == parsedString.range(of: "mno"))
+            XCTAssert(parsedElements[5].range == parsedString.range(of: "pqr"))
         } catch {
             XCTFail("Parsing failed.")
         }
@@ -116,22 +134,35 @@ class ParserTests: XCTestCase {
         }
     }
     
+    func testLiteralEqual() {
+        do {
+            let (parsedString, parsedElements) = try MarkdownParser.parse("two + two = four")
+            
+            XCTAssert(parsedString == "two + two = four")
+            XCTAssert(parsedElements.count == 0)
+        } catch {
+            XCTFail("Parsing failed.")
+        }
+    }
+    
     func testElementsInMiddleOfWords() {
         do {
-            let (parsedString, parsedElements) = try MarkdownParser.parse("the_quick_brown*fox*jumps__over__the**lazy**dog.")
+            let (parsedString, parsedElements) = try MarkdownParser.parse("the_quick_brown*fox*jumps__over__the**lazy**==dog==.")
             
             XCTAssert(parsedString == "thequickbrownfoxjumpsoverthelazydog.")
-            XCTAssert(parsedElements.count == 4)
+            XCTAssert(parsedElements.count == 5)
             
             XCTAssert(parsedElements[0].isEmElement())
             XCTAssert(parsedElements[1].isEmElement())
             XCTAssert(parsedElements[2].isStrongElement())
             XCTAssert(parsedElements[3].isStrongElement())
+            XCTAssert(parsedElements[4].isUnderlinedElement())
             
             XCTAssert(parsedElements[0].range == parsedString.range(of: "quick"))
             XCTAssert(parsedElements[1].range == parsedString.range(of: "fox"))
             XCTAssert(parsedElements[2].range == parsedString.range(of: "over"))
             XCTAssert(parsedElements[3].range == parsedString.range(of: "lazy"))
+            XCTAssert(parsedElements[4].range == parsedString.range(of: "dog"))
         } catch {
             XCTFail("Parsing failed.")
         }
@@ -163,6 +194,12 @@ class ParserTests: XCTestCase {
         } catch {
             XCTAssert(error as! ElementParser.ParserError == ElementParser.ParserError.unclosedTags)
         }
+        
+        do {
+            let _ = try MarkdownParser.parse("Not ==correct.")
+        } catch {
+            XCTAssert(error as! ElementParser.ParserError == ElementParser.ParserError.unclosedTags)
+        }
     }
     
 }
@@ -190,6 +227,15 @@ private extension MarkdownElement {
     func isStrikethroughElement() -> Bool {
         switch self {
         case .strikethrough(_):
+            return true
+        default:
+            return false
+        }
+    }
+    
+    func isUnderlinedElement() -> Bool {
+        switch self {
+        case .underline(_):
             return true
         default:
             return false
