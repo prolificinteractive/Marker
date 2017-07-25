e85643a9d4f54998830016d7e0a6b2d947d9a5c0
ThibaultKlein <thibault@prolificinteractive.com>
2017-07-25 11:03:25 -0400

    Updated UIButtonExtension to take the button state as a parameter

--- Marker/Marker/Utility/Extensions/Button/UIButtonExtension.swift
+++ Marker/Marker/Utility/Extensions/Button/UIButtonExtension.swift
@@ -16,8 +16,12 @@ public extension UIButton {
     ///   - text: The text to be displayed in the label.
     ///   - textStyle: Text style object containing style information.
     ///   - markups: Custom markup if there is any. Defaults to zero custom markup.
-    func setTitleText(_ text: String, using textStyle: TextStyle, customMarkup markups: Markup = [:]) {
-        setAttributedTitle(attributedMarkupString(from: text, using: textStyle, customMarkup: markups), for: .normal)
+    ///   - state: The button state to set.
+    func setTitleText(_ text: String,
+                      using textStyle: TextStyle,
+                      customMarkup markups: Markup = [:],
+                      forState state: UIControlState = .normal) {
+        setAttributedTitle(attributedMarkupString(from: text, using: textStyle, customMarkup: markups), for: state)
     }
     
     /// Sets the button's title text to an attributed string created from the specified string and text style.
@@ -27,8 +31,11 @@ public extension UIButton {
     /// - Parameters:
     ///   - markdownText: The Markdown text to be displayed in the label.
     ///   - textStyle: Text style object containing style information.
-    func setMarkdownTitleText(_ markdownText: String, using textStyle: TextStyle) {
-        setAttributedTitle(attributedMarkdownString(from: markdownText, using: textStyle), for: .normal)
+    ///   - state: The button state to set.
+    func setMarkdownTitleText(_ markdownText: String,
+                              using textStyle: TextStyle,
+                              forState state: UIControlState = .normal) {
+        setAttributedTitle(attributedMarkdownString(from: markdownText, using: textStyle), for: state)
     }
     
 }
