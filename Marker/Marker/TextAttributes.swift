5e62d837303765b66806da65b008e40dfae234e0
Harlan Kellaway <harlan@prolificinteractive.com>
2019-04-03 15:35:22 -0400

    Removed code check for >= Swift 4

--- Marker/Marker/TextAttributes.swift
+++ Marker/Marker/TextAttributes.swift
@@ -12,38 +12,19 @@
     import AppKit
 #endif
 
-#if swift(>=4.0)
-    /// Text attributes.
+/// Text attributes.
 public typealias TextAttributes = [NSAttributedString.Key: Any]
 
-    struct AttributedStringKey {
-
-        static let font = NSAttributedString.Key.font
-        static let foregroundColor = NSAttributedString.Key.foregroundColor
-        static let kern = NSAttributedString.Key.kern
-        static let link = NSAttributedString.Key.link
-        static let paragraphStyle = NSAttributedString.Key.paragraphStyle
-        static let strikethroughStyle = NSAttributedString.Key.strikethroughStyle
-        static let strikethroughColor = NSAttributedString.Key.strikethroughColor
-        static let underlineStyle = NSAttributedString.Key.underlineStyle
-        static let underlineColor = NSAttributedString.Key.underlineColor
-
-    }
-#else
-    /// Text attributes.
-    public typealias TextAttributes = [String: Any]
-
-    internal struct AttributedStringKey {
-
-        static let font = NSFontAttributeName
-        static let foregroundColor = NSForegroundColorAttributeName
-        static let kern = NSKernAttributeName
-        static let link = NSLinkAttributeName
-        static let paragraphStyle = NSParagraphStyleAttributeName
-        static let strikethroughStyle = NSStrikethroughStyleAttributeName
-        static let strikethroughColor = NSStrikethroughColorAttributeName
-        static let underlineStyle = NSUnderlineStyleAttributeName
-        static let underlineColor = NSUnderlineColorAttributeName
-        
-    }
-#endif
+struct AttributedStringKey {
+    
+    static let font = NSAttributedString.Key.font
+    static let foregroundColor = NSAttributedString.Key.foregroundColor
+    static let kern = NSAttributedString.Key.kern
+    static let link = NSAttributedString.Key.link
+    static let paragraphStyle = NSAttributedString.Key.paragraphStyle
+    static let strikethroughStyle = NSAttributedString.Key.strikethroughStyle
+    static let strikethroughColor = NSAttributedString.Key.strikethroughColor
+    static let underlineStyle = NSAttributedString.Key.underlineStyle
+    static let underlineColor = NSAttributedString.Key.underlineColor
+    
+}
